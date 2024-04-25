5d2bdea6bfe03373831f754c12569ceaa4af3838
dnvery <dnffx@protonmail.com>
2024-04-25 19:17:52 +0300

    Added html language server

--- home/nixvim/lsp.nix
+++ home/nixvim/lsp.nix
@@ -14,6 +14,7 @@
         lua-ls = { enable = true; };
         java-language-server = { enable = true; };
         nil_ls = { enable = true; }; # Nix
+        html = { enable = true; };
       };
       keymaps.lspBuf = {
         "gd" = "definition";
