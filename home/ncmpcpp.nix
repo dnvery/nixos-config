7cfad6352e3878e2e10f5f3bc4f501d3fbeeb448
dnvery <dnffx@protonmail.com>
2025-08-21 00:06:35 +0300

    Added yazi and missing dependency for ncmpcpp

--- home/ncmpcpp.nix
+++ home/ncmpcpp.nix
@@ -1,3 +1,13 @@
 {
+  services.mpd = {
+    enable = true;
+    musicDirectory = "~/Downloads";
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "My PipeWire Output"
+      }
+    '';
+  };
   programs.ncmpcpp.enable = true;
 }
---
+++ home/yazi.nix
@@ -0,0 +1,11 @@
+{
+  programs.yazi = {
+    enable = true;
+    settings = {
+      preview = {
+        max_width = 2000;
+        max_height = 2000;
+      };
+    };
+  };
+}
