02f06d1dcfe84a695afa1bc412b3f5707edbc073
dnvery <dnffx@protonmail.com>
2026-05-03 00:26:57 +0300

    Remove close button from GTK windows

--- home/theming.nix
+++ home/theming.nix
@@ -5,6 +5,9 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:none";
+    };
   };
 
   gtk = {
